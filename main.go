8c1663054036ecddcc3da0526d060fa23787a05e
Mugua030 <email.mugua@gmail.com>
2019-01-22 14:17:03 +0800

    hash

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
 )
 
@@ -17,14 +19,28 @@ func NewBlock(data string, preBlock []byte) *Block {
 		Hash:         nil,
 		Data:         []byte(data),
 	}
+	block.SetHash()
+
 	return &block
+}
+func (b *Block) SetHash() {
+
+	var blockInfo []byte
+	blockInfo = append(blockInfo, b.PreBlockHash...)
+	blockInfo = append(blockInfo, b.Hash...)
+	blockInfo = append(blockInfo, b.Data...)
+
+	hash := sha256.Sum256(blockInfo)
+	b.Hash = hash[:]
 }
 
 func main() {
 	block := NewBlock("Love BlockChain", []byte{})
 
 	fmt.Printf("PreBlock: %s\n", block.PreBlockHash)
-	fmt.Printf("Hash: %s\n", block.Hash)
+	//fmt.Printf("Hash: %s\n", block.Hash)
+	encodeStr := hex.EncodeToString(block.Hash)
+	fmt.Printf("Hash: %s\n", encodeStr)
 	fmt.Printf("Data: %s\n", block.Data)
 
 }
